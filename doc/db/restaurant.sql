157bdcbeb8f0523d05c1c488edf053a706551068
demon0228 <914111723@qq.com>
2018-06-27 17:47:07 +0800

    add db

---
+++ doc/db/restaurant.sql
@@ -0,0 +1,3 @@
+INSERT INTO `restaurant` VALUES (1, '大眼茶餐厅', '15821876595', 0, NULL);
+INSERT INTO `restaurant` VALUES (2, '惠粤轩', '13916178838', 0, NULL);
+INSERT INTO `restaurant` VALUES (3, '小浦东', '63030315', 0, NULL);
