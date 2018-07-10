94be04e8f3828b30a79acbf6ea010ce64027095f
hangjiaqi <q332881021@qq.com>
2018-07-10 15:17:13 +0800

    add Exception

--- springboot/src/main/java/com/cztek/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/cztek/springboot/SpringbootApplication.java
@@ -4,22 +4,25 @@ import javax.servlet.MultipartConfigElement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 
 @SpringBootApplication
+@Configuration
 public class SpringbootApplication {
-	
-	@Bean  
-    public MultipartConfigElement multipartConfigElement() {  
-        MultipartConfigFactory factory = new MultipartConfigFactory();  
+        @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
         //单个文件最大  
         factory.setMaxFileSize("10240KB"); //KB,MB  
         ///设置总上传数据总大小  
-        factory.setMaxRequestSize("1024000KB");  
-        return factory.createMultipartConfig();  
-    }  
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
---
+++ springboot/src/main/java/com/cztek/springboot/controller/BizException.java
@@ -0,0 +1,44 @@
+package com.cztek.springboot.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@Slf4j
+public class BizException {
+
+    /**
+     * 运行时异常
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler({ RuntimeException.class })
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView processException(RuntimeException exception) {
+        log.info("自定义异常处理-RuntimeException");
+        ModelAndView m = new ModelAndView();
+        m.addObject("exception", exception.getMessage());
+        m.setViewName("error/404");
+        return m;
+    }
+
+    /**
+     * Excepiton异常
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler({ Exception.class })
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView processException(Exception exception) {
+        log.info("自定义异常处理-Exception");
+        ModelAndView m = new ModelAndView();
+        m.addObject("exception", exception.getMessage());
+        m.setViewName("error/404");
+        return m;
+    }
+}
