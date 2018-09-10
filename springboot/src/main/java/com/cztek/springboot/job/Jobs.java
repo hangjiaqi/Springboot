5a24c98f13c75478d3206a09440acf99b7a5f775
hangjiaqi <q332881021@qq.com>
2018-09-10 19:22:49 +0800

    update ok

--- springboot/src/main/java/com/cztek/springboot/job/Jobs.java
+++ springboot/src/main/java/com/cztek/springboot/job/Jobs.java
@@ -31,6 +31,25 @@ public class Jobs{
         }
     }
 
+    //@Scheduled(cron = "*/5 * * * * ?")
+    @Scheduled(cron = "0 30 16 ? * MON-FRI")
+    public void remindCZ(){
+        TZemail("订餐提醒","今日订餐将于17点关闭->http://47.96.97.244:9890/");
+    }
+
+    private void TZemail(String message,String text) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        //发送者
+        mailMessage.setFrom("info@cz-tek.com");
+        //接收者
+        //mailMessage.setTo("jiaqi.hang@cz-tek.com");
+        mailMessage.setTo("rui.bai@cz-tek.com","can.cheng@cz-tek.com","chaoying.deng@cz-tek.com","yong.du@cz-tek.com","hong.gao@cz-tek.com","jiahong.guo@cz-tek.com","peixuan.gong@cz-tek.com","jiaqi.hang@cz-tek.com","haoli.li@cz-tek.com","zhongwen.lv@cz-tek.com","jing.mao@cz-tek.com","chuankui.ning@cz-tek.com","ruijie.ruan@cz-tek.com","fuzhen.song@cz-tek.com","song.wang@cz-tek.com","zimei.wu@cz-tek.com","siyuan.yang@cz-tek.com","xiaowei.wu@cz-tek.com","gumin.zhou@cz-tek.com","jie.zhao@cz-tek.com","zeyuan.zhang@cz-tek.com");
+        //发送标题
+        mailMessage.setSubject(message);
+        mailMessage.setText(text);
+        jms.send(mailMessage);
+    }
+
 
     private void email(int number,String message,String text) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
